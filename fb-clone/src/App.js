caabbdd8d3c82160ea937687f2da2211fa3e057a
Nima sherpa <Toplouie78@gmail.com>
2022-03-05 16:50:47 -0500

    adding firebase config

--- fb-clone/src/App.js
+++ fb-clone/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-	return <div className="App">Hello</div>;
+	return <div className="app">lets build the facebook app </div>;
 }
 
 export default App;
---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyDy8u6E6RAtAkY936LQaOCJGd7g_EFQMYY",
+	authDomain: "facebook-clone-b8261.firebaseapp.com",
+	projectId: "facebook-clone-b8261",
+	storageBucket: "facebook-clone-b8261.appspot.com",
+	messagingSenderId: "620907791583",
+	appId: "1:620907791583:web:fbf443f387f6016824556a",
+	measurementId: "G-KY7DKE7BWN",
+};
