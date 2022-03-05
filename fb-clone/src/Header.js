a823c7e23789c8c4efc358ab41865c3cd6e471bc
Nima sherpa <Toplouie78@gmail.com>
2022-03-05 17:43:35 -0500

    header almost completed

--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import FlagIcon from "@mui/icons-material/Flag";
+import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
+import StorefrontOutlinedIcon from "@mui/icons-material/StorefrontOutlined";
+import SupervisedUserCircleOutlinedIcon from "@mui/icons-material/SupervisedUserCircleOutlined";
+import { Avatar } from "@material-ui/core";
 
 function Header() {
 	return (
@@ -15,8 +21,29 @@ function Header() {
 					<input type="text" />
 				</div>
 			</div>
-			<div className="header__middle"></div>
-			<div className="header__right"></div>
+			<div className="header__center">
+				<div className="header__option">
+					<HomeIcon fontSize="large" />
+				</div>
+				<div className="header__option">
+					<FlagIcon fontSize="large" />
+				</div>
+				<div className="header__option">
+					<SubscriptionsOutlinedIcon fontSize="large" />
+				</div>
+				<div className="header__option">
+					<StorefrontOutlinedIcon fontSize="large" />
+				</div>
+				<div className="header__option">
+					<SupervisedUserCircleOutlinedIcon fontSize="large" />
+				</div>
+			</div>
+			<div className="header__right">
+				<div className="header__info">
+					<Avatar />
+					<h4>Nima sherpa</h4>
+				</div>
+			</div>
 		</div>
 	);
 }
