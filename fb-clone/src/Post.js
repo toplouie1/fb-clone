89017d31c1dbeffb900f52c9b18534e3c6d2b69f
Nima sherpa <Toplouie78@gmail.com>
2022-03-06 12:55:03 -0500

    some css /dummy data

--- fb-clone/src/Post.js
+++ fb-clone/src/Post.js
@@ -1,6 +1,11 @@
 import { Avatar } from "@mui/material";
 import React from "react";
 import "./Post.css";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import { ExpandMoreOutlined } from "@material-ui/icons";
 
 function Post({ profilePic, image, username, timestamp, message }) {
 	return (
@@ -17,6 +22,25 @@ function Post({ profilePic, image, username, timestamp, message }) {
 			</div>
 			<div className="post__image">
 				<img src={image} alt="" />
+			</div>
+
+			<div className="post__options">
+				<div className="post__option">
+					<ThumbUpIcon />
+					<p>Like</p>
+				</div>
+				<div className="post__option">
+					<ChatBubbleOutlineIcon />
+					<p>Comment</p>
+				</div>
+				<div className="post__option">
+					<NearMeIcon />
+					<p>Share</p>
+				</div>
+				<div className="post__option">
+					<AccountCircleIcon />
+					<ExpandMoreOutlined />
+				</div>
 			</div>
 		</div>
 	);
