a06242f12c37438b0d7ab028311465c432d7d95c
Nima sherpa <Toplouie78@gmail.com>
2022-03-05 22:59:12 -0500

    sideRow icon

--- fb-clone/src/Sidebar.js
+++ fb-clone/src/Sidebar.js
@@ -1,15 +1,32 @@
+import StorefrontOutlined from "@mui/icons-material/StorefrontOutlined";
 import React from "react";
 import "./Sidebar.css";
 import SidebarRow from "./SidebarRow";
 
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+
+import PeopleIcon from "@material-ui/icons/People";
+import ChatIcon from "@material-ui/icons/Chat";
+import StorefrontIcon from "@material-ui/icons/EmojiFlags";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+
 function Sidebar() {
 	return (
 		<div className="sidebar">
-			<SidebarRow />
-			<SidebarRow />
-			<SidebarRow />
-			<SidebarRow />
-			<SidebarRow />
+			<SidebarRow
+				src="//static0.cbrimages.com/wordpress/wp-content/uploads/2020/08/asta.jpg"
+				title="Nima"
+			/>
+			<SidebarRow
+				Icon={LocalHospitalIcon}
+				title="COVID-19 Information Center"
+			/>
+			<SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+			<SidebarRow Icon={PeopleIcon} tite="Friends" />
+			<SidebarRow Icon={ChatIcon} title="Messenger" />
+			<SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+			<SidebarRow Icon={VideoLibraryIcon} title="Videos" />
 		</div>
 	);
 }
--- fb-clone/src/SidebarRow.js
+++ fb-clone/src/SidebarRow.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./SidebarRow.css";
+import { Avatar } from "@material-ui/core";
 
-function SidebarRow() {
-	return <div className="sidebarRow">Row</div>;
+function SidebarRow({ src, Icon, title }) {
+	return (
+		<div className="sidebarRow">
+			{src && <Avatar src={src} />}
+			{Icon && <Icon />}
+			<h4>{title}</h4>
+		</div>
+	);
 }
 
 export default SidebarRow;
